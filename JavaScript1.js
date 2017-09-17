54b5e4b7e25fd489f3da04230f19b92603ea179b
Unknown <gustavorocha@inf.ufg.br>
2017-09-16 22:59:26 -0300

    Arredondar valor do IMC
    Ajustado para que o valor do imc seja arredondado para cima em duas casas decimais

--- JavaScript1.js
+++ JavaScript1.js
@@ -2,5 +2,5 @@ function calcImc(){
     var peso =parseFloat(document.getElementById("pesoId").value);
     var altura =parseFloat(document.getElementById("alturaId").value);
     var imc = peso / (altura*altura);
-    alert("O seu IMC é: "+imc);
+    alert("O seu IMC é: "+imc.toFixed(2));
 }

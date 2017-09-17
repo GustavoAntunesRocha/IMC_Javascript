911656532da6597b5d60cffa1a69236ed61efa12
Unknown <gustavorocha@inf.ufg.br>
2017-09-16 23:17:55 -0300

    Adicionado classificações para o imc
    Adicionado as classificações de acordo com o imc

--- JavaScript1.js
+++ JavaScript1.js
@@ -1,6 +1,30 @@
 function calcImc(){
     var peso =parseFloat(document.getElementById("pesoId").value);
     var altura =parseFloat(document.getElementById("alturaId").value);
-    var imc = peso / (altura*altura);
-    alert("O seu IMC é: "+imc.toFixed(2));
+    var temp = peso / (altura*altura);
+    var imc = temp.toFixed(2);
+    if(imc < 16){
+        alert("Seu imc é: " + imc + "\nMagreza grave!")
+    }
+    else if(imc < 17){
+        alert("Seu imc é: " + imc + "\nMagreza moderada!")
+    }
+    else if(imc < 18.5){
+        alert("Seu imc é: " + imc + "\nMagreza leve!")
+    }
+    else if(imc < 25){
+        alert("Seu imc é: " + imc + "\nVocê está saudável!")
+    }
+    else if(imc < 30){
+        alert("Seu imc é: " + imc + "\nSobrepeso!")
+    }
+    else if(imc < 35){
+        alert("Seu imc é: " + imc + "\nObesidade Grau 1!")
+    }
+    else if(imc < 40){
+        alert("Seu imc é: " + imc + "\nObesidade Grau 2 (severa)!")
+    }
+    else{
+        alert("Seu imc é: " + imc + "\nObesidade Grau 3 (mórbida)!")
+    }
 }
